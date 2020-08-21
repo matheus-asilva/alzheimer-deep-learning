a40198860407f0dd4eef65f22fca48768e6a99c1
Matheus de Almeida Silva <ms.asilvas1@gmail.com>
2020-08-21 09:00:29 -0300

    Add files via upload

---
+++ architecture/networks/mobilenet.py
@@ -0,0 +1,28 @@
+from typing import Tuple
+from tensorflow.keras.applications import MobileNet
+from tensorflow.keras.layers import AveragePooling2D, Flatten, Dense, Dropout, Input
+from tensorflow.keras.models import Model
+
+
+def mobilenet(input_shape: Tuple[int, int, int], output_shape: Tuple[int, ...], weights: str='imagenet', include_top: bool=False) -> Model:
+    base_model = MobileNet(weights=weights, include_top=include_top, input_tensor=Input(shape=input_shape))
+    if include_top:
+        return base_model
+    else:
+        num_classes = output_shape[0]
+
+        head_model = base_model.output
+        head_model = AveragePooling2D(pool_size=(4,4))(head_model)
+        head_model = Flatten(name='flatten')(head_model)
+        head_model = Dense(64, activation='relu')(head_model)
+        head_model = Dropout(0.5)(head_model)
+        head_model = Dense(num_classes, activation='softmax')(head_model)
+
+        # Place the head Fully Connected model on top of the base model (actual model)
+        model = Model(base_model.input, head_model)
+
+        # Loop over all layers in the base model and freeze them so they won't be updated during the first training process
+        for layer in base_model.layers:
+            layer.trainable = False
+        
+        return model
