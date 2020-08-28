21975600d33babb4358642560f7b60fa7636e1ea
matheus-asilva <ms.asilvas1@gmail.com>
2020-08-27 23:10:48 -0300

    run all architectures

--- tasks/run_all_architectures.sh
+++ tasks/run_all_architectures.sh
@@ -1,10 +1,9 @@
 set PYTHONPATH=. &&
 wandb login 825e234f13b80286db418be6ccf93e748593dce7 &&
-# já foi mobilenet
-for net in vgg16 vgg19 xception resnet152v2 resnet50v2
+for net in mobilenet vgg16 vgg19 xception resnet152v2 resnet50v2
 do
-    echo "############################################################################################"
+    echo "###################################################################################################################################################"
     echo "Training $net..."
-    python training\\run_experiment.py --save "{\"dataset\": \"AlzheimerT2StarSmallDataset\", \"dataset_args\": {\"types\": [\"CN\", \"AD\"]}, \"model\": \"AlzheimerCNN\", \"network\": \"$net\", \"train_args\": {\"batch_size\": 16, \"epochs\": 100}, \"opt_args\": {\"lr\": 1e-3, \"decay\": 1e-5}}"
+    python training\\run_experiment.py --save "{\"dataset\": \"AlzheimerT2StarFullDataset\", \"dataset_args\": {\"types\": [\"CN\", \"AD\"]}, \"model\": \"AlzheimerCNN\", \"network\": \"$net\", \"train_args\": {\"batch_size\": 8, \"epochs\": 100}, \"opt_args\": {\"lr\": 1e-3, \"decay\": 1e-5}}"
     echo " "
 done
