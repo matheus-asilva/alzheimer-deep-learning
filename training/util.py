938e62b75f43133b4358a312cbd3a37cf01f7937
matheus-asilva <ms.asilvas1@gmail.com>
2020-09-18 19:39:22 -0300

    make confusion_matrix plot

--- training/util.py
+++ training/util.py
@@ -8,6 +8,10 @@ from wandb.keras import WandbCallback
 from architecture.datasets.dataset import Dataset
 from architecture.models.base import Model
 
+from sklearn.metrics import confusion_matrix
+import itertools
+import matplotlib.pyplot as plt
+
 import numpy as np
 
 EARLY_STOPPING = True
@@ -51,3 +55,37 @@ def train_model(model: Model, dataset: Dataset, epochs: int = 10, batch_size: in
     print('Training took {:2f} minutes'.format((time() - t) / 60))
 
     return model
+
+def plot_confusion_matrix(model, X, y, classes, batch_size=8, normalize=False, title='Confusion Matrix', cmap=plt.cm.Blues):
+    
+    if len(y[0]) > 1:
+        y = np.argmax(y, axis=1)
+    
+    y_preds = model.predict(X=X, batch_size=batch_size)
+
+    cm = confusion_matrix(y_true=y, y_pred=y_preds)
+    
+    g = plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+    return g
