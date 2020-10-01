27bdf20747a0eebd6ceff357a946d49b2262f891
Matheus de Almeida Silva <ms.asilvas1@gmail.com>
2020-09-30 22:36:33 -0300

    update

--- tasks/run_many_architectures.sh
+++ tasks/run_many_architectures.sh
@@ -1,10 +1,10 @@
 export PYTHONPATH=. &&
 wandb login 825e234f13b80286db418be6ccf93e748593dce7 &&
 # for net in mobilenet vgg16 vgg19 xception resnet152v2 resnet50v2
-for net in full_mobilenet
+for net in mobilenet
 do
     echo "Training $net..."
-    python training/run_experiment.py --nowandb --save "{\"dataset\": \"AlzheimerMPRageNoDeep\", \"dataset_args\": {\"types\": [\"CN\", \"AD\"]}, \"model\": \"AlzheimerCNN\", \"network\": \"$net\", \"train_args\": {\"batch_size\": 4, \"epochs\": 100}, \"opt_args\": {\"learning_rate\": 1e-3}}"
+    python training/run_experiment.py --save "{\"dataset\": \"AlzheimerMPRageNoDeep\", \"dataset_args\": {\"types\": [\"CN\", \"AD\"]}, \"model\": \"AlzheimerCNN\", \"network\": \"$net\", \"train_args\": {\"batch_size\": 32, \"epochs\": 100}, \"opt_args\": {\"learning_rate\": 1e-3}}"
     echo " "
 done
 
